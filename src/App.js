919176411dfa51178d053db1624b819d9f50fe0c
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-03-21 20:43:13 +0600

    data loaded done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      
+      <LoadCountries></LoadCountries>
     </div>
   );
 }
-
+function LoadCountries(){
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountries(data));
+  }, [])
+  return (
+    <div>
+      <h1>Load rest countries details!</h1>
+      <h4>Available countries: {countries.length}</h4>
+    </div>
+  )
+}
 export default App;
